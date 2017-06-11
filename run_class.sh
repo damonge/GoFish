5a6aca814e4b66d3d3d7fa32d005e6d8795eac5a
Siddharth Mishra Sharma <smsharma@princeton.edu>
2017-06-11 00:38:53 -0400

    Running with lmax=5000

--- run_class.sh
+++ run_class.sh
@@ -4,8 +4,28 @@ if [ "$CLUSTER" == "tiger" ]; then
 cat > submit_class.batch <<EOF
 #!/bin/bash
 #SBATCH -N 1   # node count
-#SBATCH --ntasks-per-node=16
-#SBATCH -t 01:05:00
+#SBATCH --ntasks-per-node=12
+#SBATCH -t 02:05:00
+#SBATCH --mem=48gb
+##SBATCH --mail-type=begin
+##SBATCH --mail-type=end
+##SBATCH --mail-user=smsharma@princeton.edu
+
+# MPI compilers
+export PATH="/tigress/smsharma/anaconda2/bin:$PATH"
+source activate venv_py27
+module load openmpi/gcc/1.8.8/64
+
+cd /tigress/smsharma/GoFish/
+
+./class_mod $1
+EOF
+elif [ "$CLUSTER" == "della" ]; then
+cat > submit_class.batch <<EOF
+#!/bin/bash
+#SBATCH -N 1   # node count
+#SBATCH --ntasks-per-node=12
+#SBATCH -t 02:05:00
 #SBATCH --mem=48gb
 ##SBATCH --mail-type=begin
 ##SBATCH --mail-type=end
@@ -25,8 +45,8 @@ cat > submit_class.batch <<EOF
 #!/bin/bash
 #SBATCH -N 1   # node count
 #SBATCH --ntasks-per-node=16
-#SBATCH -t 01:05:00
-#SBATCH --mem=48gb
+#SBATCH -t 02:05:00
+#SBATCH --mem=64gb
 ##SBATCH --mail-type=begin
 ##SBATCH --mail-type=end
 ##SBATCH --mail-user=smsharma@princeton.edu
